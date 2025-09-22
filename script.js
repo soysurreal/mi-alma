56f4296dbfb24c161039df5f057d2f63cc0b1127
soysurreal <ISA.HERRERA0408@GMAIL.COM>
2025-09-22 02:35:32 +0000

    LIMPIEZA 2

--- script.js
+++
@@ -1,4 +0,0 @@
-const imagenes = [
-  "https://www.postposmo.com/wp-content/uploads/2020/09/Especies-end%C3%A9micas-de-colombia-3.jpg",
-  "https://www.postposmo.com/wp-content/uploads/2020/09/Especies-end%C3%A9micas-de-colombia-4.jpg",
-];
